6d8d62e666d0a8d6e7ab0a4522a8d1260dfe95e6
Azamat <azamatfdgfr@gmail.com>
2023-09-23 17:54:18 +0600

    fasfasffas

--- slider.js
+++ slider.js
@@ -1,5 +1,4 @@
 let indexPrev = 0;
-let Prev = document.querySelector('.Prev');
 const Merlot1 = document.querySelector('.Merlot1');
 const Merlot2 = document.querySelector('.Merlot2');
 const Merlot3 = document.querySelector('.Merlot3');
@@ -9,7 +8,7 @@ function PR() {
    console.log(indexPrev)
    if (indexPrev == 1) {
       Merlot1.style.opacity = "1"
-      Merlot1.style.transform = "translate(274px, 0px)"
+      Merlot1.style.transform = "translate(257px, 0px)"
       Merlot2.style.opacity = "0"
       Merlot2.style.transform = "translate(117px, 0px)"
       Merlot3.style.transform = "translate(-405px, 0px)"
@@ -17,7 +16,7 @@ function PR() {
    }
    if (indexPrev == 2) {
       Merlot3.style.opacity = "1"
-      Merlot3.style.transform = "translate(-286px, 0px)"
+      Merlot3.style.transform = "translate(-255px, 0px)"
       Merlot1.style.opacity = "0"
       Merlot1.style.transform = "translate(389px, 0px)"
       Merlot2.style.transform = "translate(-228px, 0px)"
@@ -32,7 +31,6 @@ function PR() {
       Merlot2.style.opacity = "1"
    }
 }
-let Next = document.querySelector('.Next');
 let indexNext = 0;
 function NT() {
    indexPrev = 0
@@ -40,7 +38,7 @@ function NT() {
    console.log(indexNext)
    if (indexNext == 1) {
       Merlot3.style.opacity = "1"
-      Merlot3.style.transform = "translate(-273px, 0px)"
+      Merlot3.style.transform = "translate(-255px, 0px)"
       Merlot2.style.transform = "translate(-138px, 0px)"
       Merlot2.style.opacity = "0"
       Merlot1.style.transform = "translate(459px, 0px)"
@@ -48,7 +46,7 @@ function NT() {
    }
    if (indexNext == 2) {
       Merlot2.style.transform = "translate(200px, 0px)"
-      Merlot1.style.transform = "translate(274px, 0px)"
+      Merlot1.style.transform = "translate(257px, 0px)"
       Merlot3.style.transform = "translate(-405px, 0px)"
       Merlot1.style.opacity = "1"
       Merlot3.style.opacity = "0"
@@ -62,6 +60,33 @@ function NT() {
       Merlot3.style.transform = "translate(-104px, 0px)"
       Merlot2.style.transform = "translate(0px, 0px)"
       Merlot1.style.transform = "translate(146px, 0px)"
+   }
+}
+let cards = document.querySelector('.cards');
+let futured = document.querySelector('.Futured_Products');
+let cardV = document.querySelector('.cardV')
+let  cardE = document.querySelector('.cardE')
+let  cardT = document.querySelector('.cardT')
+let  cardR = document.querySelector('.cardR')
+let indexView = 0 
+function  View() {   
+   ++indexView
+   if (indexView== 1) {
+      cardV.className ="card"
+      cardE.className ="card"
+      cardT.className ="card"
+      cardR.className ="card"
+      cards.style.height ="1454px" 
+      futured.style.height ="1831px" 
+   }
+   else{
+      cardV.className ="cardV"
+      cardE.className ="cardE"
+      cardT.className ="cardT"
+      cardR.className ="cardR"
+      cards.style.height ="950px" 
+      futured.style.height ="1246px" 
+      indexView=0
    }
 }
 
